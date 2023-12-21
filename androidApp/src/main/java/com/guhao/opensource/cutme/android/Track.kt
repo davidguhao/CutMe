1dab3abca2334160d7acb66829916c09f8e42976
古灏 <gulizhiguhao@gmail.com>
2023-12-22 01:01:55 +0800

    [Bug] Forget to set a PieceCard alpha to 0.99f

--- androidApp/src/main/java/com/guhao/opensource/cutme/android/Track.kt
+++ androidApp/src/main/java/com/guhao/opensource/cutme/android/Track.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
@@ -210,6 +211,7 @@ fun BlankPiece(
         ) {
             PieceCard(
                 modifier = Modifier
+                    .alpha(0.99f)
                     .height(pieceHeight)
                     .width(blankPieceWidth.dp),
                 content = {}
